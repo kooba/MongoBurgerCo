eb4e0c684bbba2f7439e910b03a2fe4e4c5eb3e8
Jakub Borys <jakub.borys@gmail.com>
2013-04-15 07:32:11 +0100

    Express Server added.

--- mburger.js
+++ mburger.js
@@ -1,6 +1,25 @@
 var app = require('./lib/app');
 
-app.init(function(err, message){
+params = {
+    config: {
+        project: 'Mongo Burger Company'
+//        public: '/../public',
+//        per_page: '10'
+    },
+    dev: {
+        user: 'user',
+        pass: 'password',
+        port: '3000',
+        db:   'mongodb://localhost/MongoBurgerCo'
+    },
+    prod: {
+        user: 'user',
+        pass: 'password',
+        db:   'mongodb://localhost/MongoBurgerCo'
+    }
+}
+
+app.init(params, function(err, message){
     if(err){
         console.log('Failed to initialized app, ' + err.message);
     } else{
